7fcf2a47c6c611fc17ba5b5d2eceb45b255ad934
Emily Tuttle <emilytuttle00@gmail.com>
2024-12-01 02:02:55 -0700

    team3

--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,4 @@
-import { setLocalStorage, getLocalStorage, getParam } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage, getParam, loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
@@ -6,6 +6,8 @@ const productId = getParam("product");
 const dataSource = new ProductData("tents");
 
 const product = new ProductDetails(productId, dataSource);
+
+loadHeaderFooter()
 product.init();
 
 // // ADD TO CART
